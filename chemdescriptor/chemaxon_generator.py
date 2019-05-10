6b3e2a74af1ee93faf30611005ab38a5a3f4317d
vshekar <gv.shekar@gmail.com>
2019-05-10 16:15:21 -0400

    Added exception handling and input sanitization

--- chemdescriptor/chemaxon_generator.py
+++ chemdescriptor/chemaxon_generator.py
@@ -49,8 +49,8 @@ class ChemAxonDescriptorGenerator(BaseDescriptorGenerator):
             input_molecule_file_path:   path to ip molecules
             descriptor_file_path:       path to ip descriptors
             ph_values:                  list of pH values at which to calculate descriptors
-            command_stems:              Dictonary of command stems and their name
-            ph_command_stems:           Dict of pH related command stems
+            command_stems:              Dictonary of descriptors and its command stem
+            ph_command_stems:           Dict of pH related descriptors and command stems
         """
         super().__init__(input_molecule_file_path, descriptor_file_path)
 
@@ -86,76 +86,97 @@ class ChemAxonDescriptorGenerator(BaseDescriptorGenerator):
     def _setup_descriptor_commands(self):
         """
         Converts entries in the descriptor whitelist to the corresponding commands.
-        Commands are in list form even if there is one keyword in command
-        because it is easier to chain when running cxcalc
+        Checks if commands are in list form, if not, convert
 
         """
-
         if self._command_stems:
             for descriptor in self.descriptors:
                 if type(self._command_stems[descriptor]) is list:
                     self._command_dict[descriptor] = self._command_stems[descriptor]
+                elif type(self._command_stems[descriptor]) is str:
+                    self._command_dict[descriptor] = \
+                        self._command_stems[descriptor].split()
                 else:
-                    self._command_dict[descriptor] = [
-                        self._command_stems[descriptor]]
+                    raise Exception('Command for descriptor {} should be list or str.'
+                                    'Found {}'.format(descriptor, type(self._command_stems[descriptor])))
         else:
             for cmd in self.descriptors:
                 # Cannot use dict comprehension, because OrderedDict
-                self._command_dict[cmd] = [cmd]
+                if type(cmd) is list:
+                    self._command_dict[cmd] = cmd
+                elif type(cmd) is str:
+                    self._command_dict[cmd] = cmd.split()
+                else:
+                    raise Exception('Command for descriptor {} should be list or str.'
+                                    'Found {}'.format(cmd, type(cmd)))
 
     def _setup_ph_descriptor_commands(self):
         """
         Generates and adds ph specific descriptors to the final
         list of descriptors.
-        Also generates the appropriate commands related to the pH
-        descriptors
+
         """
 
         for ph_desc in self.ph_descriptors:
             command = self._ph_command_stems[ph_desc]
-            if type(command) is list:
+            nominal_copy = copy.copy(command)
+            if type(nominal_copy) is list:
                 self._command_dict["{}_nominal".format(
-                    ph_desc)] = copy.copy(command)
+                    ph_desc)] = nominal_copy
+            elif type(nominal_copy)is str:
+                self._command_dict["{}_nominal".format(ph_desc)] = \
+                    nominal_copy.split()
             else:
-                self._command_dict["{}_nominal".format(ph_desc)] = [
-                    copy.copy(command)]
+                raise Exception('Command for descriptor {} should be list or str.'
+                                'Found {}'.format(ph_desc, type(nominal_copy)))
 
             for pH in self.ph_values:
                 ph_command = copy.copy(command)
                 pH_string = str(pH).replace('.', '_')  # R compatibility
                 if type(ph_command) is list:
                     ph_command.extend(['-H', str(pH)])
+                elif type(ph_command) is str:
+                    ph_command = ph_command.split() + ['-H', str(pH)]
                 else:
-                    ph_command = [ph_command, '-H', str(pH)]
+                    raise Exception('Command for descriptor {} should be list or str.'
+                                    'Found {}'.format(ph_desc, type(ph_command)))
                 self._command_dict["{}_ph{}".format(
                     ph_desc, pH_string)] = ph_command
 
     def generate(self, output_file_path):
         """
         Method called to generate descriptors
-        TODO: Find out why lec is calculated
+        Calculates Least Energy Conformer (LEC) for each molecule by cxcalc and then writes
+        into a temporary output file called lec_molecules.txt which is removed after the 
+        descriptors are generated. File is written in the same folder as the desired output
+        file. Inelegant solution because there is no way to directly share data with cxcalc
+
+        Possible to use tempfile module in python?
+
         Args:
             output_file_path : Path to the desired output file
 
         """
         output_folder, output_file = os.path.split(output_file_path)
         intermediate_file = os.path.join(output_folder, 'lec_molecules.txt')
-        self.generate_lec(intermediate_file)
-        self.generate_descriptors(
-            intermediate_file, output_file_path)
+        try:
+            self.generate_lec(intermediate_file)
+            self.generate_descriptors(
+                intermediate_file, output_file_path)
+        except:
+            if os.path.exists(intermediate_file):
+                # Clean up intermediate file if an exception occurs
+                os.remove(intermediate_file)
         os.remove(intermediate_file)
 
     def generate_lec(self, output_file):
         """
-        Method to filter out molecular structures with lowest
-        energy conformer? Runs cxcalc on given molecules with leconformer
-        flag and selects molecules whose structure is lec
+        Method to generate LEC. Runs cxcalc on given molecules with leconformer
+        flag and outputs the LEC for each molecule
 
         Args:
             output_folder: Writable folder to which the intermediate file
             is written
-
-        TODO: Verify contents of output file and parse accordingly
 
         """
 
@@ -171,13 +192,13 @@ class ChemAxonDescriptorGenerator(BaseDescriptorGenerator):
         to an intermediate file which is read by this script.
 
         Args:
-            smiles_molecules:   List of SMILES molecules. Ideally
-                                passed through generate_lec
+            smiles_molecules:   Path to file of SMILES molecules or LEC molecules
+                                Ideally passed through generate_lec
             output_filename:    Path to output file
 
         """
         _command_list = list(chain.from_iterable(self._command_dict.values()))
-        print(_command_list)
+
         calcProc = subprocess.run([os.path.join(self.CXCALC_PATH, 'cxcalc'),
                                    '-o', output_filename, smiles_molecules] +
                                   _command_list)
@@ -189,7 +210,7 @@ class ChemAxonDescriptorGenerator(BaseDescriptorGenerator):
     def _parse_output(self, filename):
         """
         Parses the output file generated by cxcalc and stored in a
-        data structure defined by us.
+        data structure defined by user.
         Currently converted to pandas dataframe and written into csv.
         First column is replaced by smiles codes and column names are changed
         to whatever is defined in the input descriptors json
@@ -201,8 +222,9 @@ class ChemAxonDescriptorGenerator(BaseDescriptorGenerator):
 
         df = pd.read_csv(filename, sep='\t')
         df['Compound'] = self.smiles
+        # Replace First column with Compound
         df = df[['Compound'] + [c for c in df if c not in ['id', 'Compound']]]
-
+        # Replace cxcalc generated labels with the ones defined in descriptor dict
         df.columns = ['Compound'] + \
             [label for label in self._command_dict.keys()]
         df.to_csv(filename, index=False)
